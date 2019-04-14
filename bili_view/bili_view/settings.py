4a1e66291a36a720cd7ffa2c84c0ab8fe17f3820
wei <wm@qq.com>
2019-04-14 16:49:22 +0800

    right settings.py

--- bili_view/bili_view/settings.py
+++ bili_view/bili_view/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATIC_ROOT = '/home/bilidata/static'
+STATIC_ROOT = '/home/biliproject/static'
 
 
 MEDIA_URL = '/media/'
